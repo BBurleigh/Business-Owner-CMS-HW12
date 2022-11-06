bb9d68e196df3a398c495aa7ae7ec118c6121406
Brayden <brayburl224@gmail.com>
2022-11-06 16:38:16 -0500

    beginning to work on a function that will provide prompts to the user in the terminal --> may rewrite some of this so that a const is passed to the inquirer.prompt later

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { default: inquirer } = require('inquirer');
 
 const mysql = require('mysql2');
 
@@ -20,6 +21,19 @@ const db = mysql.createConnection(
     console.log(`You are connected to the company_db.`)
 )
 
+app.use((req, res) => {
+    res.status(404).end();
+})
+
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`); 
 })
+
+function terminalPrompts() {
+    inquirer.prompt({
+        name: "options",
+        type: "list",
+        message: "You have accessed your guild's database. What would you like to do? Scroll up/down for more options.",
+        choices: ["View all factions of your guild (departments).", "View all guild members (employees).", "View all guild members by their factions."]
+    })
+}
