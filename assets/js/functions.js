1cf5427ead6c8542f5305a60fea48b749826a0e5
Brayden <brayburl224@gmail.com>
2022-11-08 08:30:53 -0500

    created asets folder with js file for functions + renamed a few pieces of the schema.sql

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+function factionDirectory() {
+    const sql = `SELECT * FROM FACTIONS`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        console.table(rows);
+        return terminalPrompts();
+    })
+}
